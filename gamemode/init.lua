c44c5d42cf451bff31738f64010fb71f832bae8a
SpikeyScout <34626346+SpikeyScout@users.noreply.github.com>
2022-06-28 01:31:00 -0400

    h

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,7 +4,11 @@ AddCSLuaFile( "bothud.lua" )
 AddCSLuaFile( "scoreboard.lua" )
 
 include( "shared.lua" )
+include( "simple_nextbot.lua" )
+
 print("hello world!")
+
+local model = {"models/player/tfa_lwa_sucy_uni.mdl", "models/player/alyx.mdl", "models/player/barney.mdl", "models/player/breen.mdl", "models/player/charple.mdl", "models/player/p2_chell.mdl", "models/player/combine_soldier.mdl", "models/player/combine_super_soldier.mdl", "models/player/combine_soldier_prisonguard.mdl", "models/player/combine_soldier_prisonguard.mdl", "models/player/eli.mdl", "models/player/Group01/female_01.mdl", "models/player/Group01/female_02.mdl", "models/player/Group01/female_03.mdl", "models/player/Group01/female_04.mdl", "models/player/Group01/female_05.mdl", "models/player/Group01/female_06.mdl", "models/player/Group03/female_07.mdl", "models/player/Group03/female_08.mdl", "models/player/Group03/female_09.mdl", "models/player/Group03/female_10.mdl", "models/player/Group03/female_11.mdl", "models/player/Group03/female_12.mdl", "models/player/Group03/male_01.mdl", "models/player/Group03/male_02.mdl", "models/player/Group03/male_03.mdl", "models/player/Group03/male_04.mdl", "models/player/Group03/male_05.mdl", "models/player/Group03/male_06.mdl", "models/player/Group03/male_07.mdl", "models/player/Group03/male_08.mdl", "models/player/Group03/male_09.mdl"}
 
 function GM:PlayerSpawn(ply)
     ply:SetMaxHealth(100)
@@ -13,8 +17,13 @@ function GM:PlayerSpawn(ply)
     ply:Give("weapon_fists")
     ply:Give("parkourmod")
     ply:SetupHands()
+    ply:SetModel(model[ math.random( #model )])
 end
+
+
 
 hook.Add( "PlayerSwitchFlashlight", "BlockFlashLight", function( ply, enabled )
 	return ply:Alive() -- this is the only way i can get the flashlight to work lmao
 end )
+
+
