94084c38b5dd8bda9403bad09906b2664230a2e4
SpikeyScout <34626346+SpikeyScout@users.noreply.github.com>
2022-06-27 22:30:33 -0400

    i love lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,13 +1,20 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 AddCSLuaFile( "bothud.lua" )
+AddCSLuaFile( "scoreboard.lua" )
 
 include( "shared.lua" )
 print("hello world!")
 
 function GM:PlayerSpawn(ply)
     ply:SetMaxHealth(100)
-    ply:Give("parkourmod")
+    ply:SetWalkSpeed(200)
+    ply:SetRunSpeed(400)
     ply:Give("weapon_fists")
+    ply:Give("parkourmod")
     ply:SetupHands()
 end
+
+hook.Add( "PlayerSwitchFlashlight", "BlockFlashLight", function( ply, enabled )
+	return ply:Alive() -- this is the only way i can get the flashlight to work lmao
+end )
---
+++ gamemode/scoreboard.lua
@@ -0,0 +1,67 @@
+local ScoreboardDerma = nil
+local PlayerList = nil
+
+function GM:ScoreboardShow()
+
+if !IsValid(ScoreboardDerma) then
+    ScoreboardDerma = vgui.Create("DFrame")
+    ScoreboardDerma:SetSize(750, 500)
+    ScoreboardDerma:SetPos(ScrW() / 2 - 325, ScrH() / 2 - 250)
+    ScoreboardDerma:SetTitle("Nextbot Survival")
+    ScoreboardDerma:SetDraggable(false)
+    ScoreboardDerma:ShowCloseButton(false)
+    ScoreboardDerma.Paint = function()
+        draw.RoundedBox(10, 0, 0, ScoreboardDerma:GetWide(), ScoreboardDerma:GetTall(), Color(50, 50, 50, 200))
+    end
+
+    local PlayerScrollPanel = vgui.Create("DScrollPanel", ScoreboardDerma)
+    PlayerScrollPanel:SetSize(ScoreboardDerma:GetWide(), ScoreboardDerma:GetTall() - 20)
+    PlayerScrollPanel:SetPos(0, 20)
+
+    PlayerList = vgui.Create("DListLayout", PlayerScrollPanel)
+    PlayerList:SetSize(PlayerScrollPanel:GetWide(), PlayerScrollPanel:GetTall())
+    PlayerList:SetPos(0, 0)
+end
+    if IsValid(ScoreboardDerma) then
+        PlayerList:Clear()
+
+        local PLAYERS = player.GetAll()
+        table.sort( PLAYERS, function ( a, b ) return a:Deaths() > b:Deaths() end )
+        for k, v in pairs(PLAYERS) do
+            local PlayerPanel = vgui.Create("DPanel", PlayerList)
+            local Avatar = vgui.Create("AvatarImage", PlayerPanel)
+            Avatar:SetSize(32, 32)
+            Avatar:SetPlayer( v, 64)
+            Avatar:SetPos(0, 5)
+            PlayerPanel:SetSize(PlayerList:GetWide(), 50)
+            PlayerPanel:SetPos(0, 0)
+            PlayerPanel.Paint = function()
+                draw.RoundedBox(0, 0, 0, PlayerPanel:GetWide(), PlayerPanel:GetTall(), Color(40, 40, 40, 200))  
+                draw.RoundedBox(0, 0, 49, PlayerPanel:GetWide(), 1, Color(255, 255, 255, 255))
+
+                draw.SimpleText(v:GetName(), "DermaDefault", 35, 10, Color(255,255,255))
+                draw.SimpleText("Deaths: "..v:Deaths(), "DermaDefault", PlayerList:GetWide() - 20, 10, Color(255, 255, 255), TEXT_ALIGN_RIGHT)
+                draw.SimpleText("Kills:"..v:Frags(), "DermaDefault", PlayerList:GetWide() - 20, 25, Color(255, 255, 255), TEXT_ALIGN_RIGHT)
+                
+                
+                end
+                local addbutton = vgui.Create("DButton", PlayerPanel)
+                addbutton:SetPos( PlayerList:GetWide() - 100, 25 )
+                addbutton:SetSize( 100, 20 )
+                addbutton:SetText( "Open Steam profile" )
+                addbutton.DoClick = function()
+                    v:ShowProfile()
+            end
+        end
+
+        ScoreboardDerma:Show()
+        ScoreboardDerma:MakePopup()
+        ScoreboardDerma:SetKeyboardInputEnabled(false)
+    end
+end 
+function GM:ScoreboardHide()
+    if IsValid(ScoreboardDerma) then
+        ScoreboardDerma:Hide()
+    end
+end
+
